f54c798fbb62b3001c0ca2aee98b6268078ec6d0
FaroukGad <farouk5.gad@gmail.com>
2017-02-26 00:56:38 +0100

    home

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+@include('style/header.html');
+?>
+
+<style>
+.background{
+    background:url('img/569476.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+
+<body class="background ">
+<div class="container ">
+    <div class="col-lg-6">
+    <h1 class="titre_text">Donjon et Dragon</h1>
+    <a href="select.php"><button>Nouvelle partie</button></a>
+    </div> 
+</div>
+
+
